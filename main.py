cf34812e0d6c6678cb517f96a08b76938727f7e6
sepydev <spr.864.2485s@gmail.com>
2025-01-14 18:15:05 +0330

    repository

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-def main():
-    print("Hello World:)") 
-    return 2
+from telegram.ext import Application, CommandHandler
+from config import BOT_TOKEN
+from users import start_user_management
 
-print(main())
-print("by World:(")
+async def main():
+    application = Application.builder().token(BOT_TOKEN).build()
+
+    # Register handlers
+    start_user_management(application)
+
+    await application.start()
+    await application.start_polling()
+    await application.idle()
+
+if __name__ == '__main__':
+    import asyncio
+    asyncio.run(main())
+    #sss
